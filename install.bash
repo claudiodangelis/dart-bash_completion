4cf8529fd1be4baa3609fb1e8dd289ca364b2456
Robert Åkerblom-Andersson <Robert@dartvoid.com>
2014-08-25 14:33:03 +0200

    Added install script

---
+++ install.bash
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+LIN_OSX_PATH="/etc/bash_completion.d/"
+
+# List of completion files 
+DART_CMDS="dart dart2js dartanalyzer dartfmt docgen pub"
+
+if [[ "$OSTYPE" == "linux-gnu" ]] || [[ "$OSTYPE" == "darwin"* ]] ; then
+  echo "Installing bash completion for Dart commands..."
+  
+  # Check if bash-completion is installed 
+  if [[ "$OSTYPE" == "linux-gnu" ]] ; then
+    # If debian based Linux dist 
+    if [ -f /etc/debian_version ]; then
+      dpkg -s bash-completion > /dev/null
+      if [ $? != 0 ] ; then 
+        # Install if not already installed 
+        sudo apt-get install -y bash-completion
+      fi
+    else 
+      read -p "Make sure you have the bash-completion package installed before continuing (press enter to continue, Ctrl+C to cancel)"
+    fi
+  else 
+    # TODO: Add check with brew...
+    echo "OSX CHECK" > /dev/null
+  fi
+  
+  # Install completion scripts 
+  for cmd in $DART_CMDS ; do 
+    sudo cp ${cmd} ${LIN_OSX_PATH}
+  done
+  
+  echo "Done!"  
+else 
+  echo "Unsupported OS"
+fi
+
